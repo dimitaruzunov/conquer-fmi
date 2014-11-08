6f3bea14be0f57cf6e9631525bca873660680135
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2014-11-08 13:58:48 +0200

    index form now redirects to /game/{username}

--- public/scripts/events.js
+++ public/scripts/events.js
@@ -1,6 +1,12 @@
 (function () {
-	$("body").on("click", ".territory .free", function() {
+	var body = $("body");
+	body.on("click", ".territory .free", function() {
 		attack($(this).attr("id"));
 		$(this).addClass("underAttack");
+	});
+
+	body.on("click", "#btn-join", function(e) {
+		e.preventDefault();
+		window.location.href = $(this).attr("href") + "/" + $("#username").val();
 	});
 })();
